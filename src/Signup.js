85fdefbb81646669a1baaca037f043673546a017
jagadhis <jwaran78@gmail.com>
2022-09-09 11:12:24 +0530

    Creating sign up pager

---
+++ src/Signup.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Signup() {
+  return (
+    <form>
+        <div>
+           <input id="firstName" name="firstName" />
+        </div>
+    </form>
+    
+  )
+}
+
+export default Signup
