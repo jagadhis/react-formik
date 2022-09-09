2108706fec1639ee207b4d340a307c0e23cb1449
jagadhis <jwaran78@gmail.com>
2022-09-09 11:12:54 +0530

    Input field

--- src/Signup.js
+++ src/Signup.js
@@ -4,7 +4,7 @@ function Signup() {
   return (
     <form>
         <div>
-           <input id="firstName" name="firstName" />
+           <input id="firstName" name="firstName" type="text" placeholder="Enter your name" />
         </div>
     </form>
     
