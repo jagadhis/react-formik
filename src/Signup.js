c98c878197f38e825b36fc6ce8c3c731529903b4
jagadhis <jwaran78@gmail.com>
2022-09-09 17:51:16 +0530

    onChange

--- src/Signup.js
+++ src/Signup.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Signup() {
+
+  const [firstName,setFirstName] = useState("")
+
+  console.log(firstName);
   return (
     <form>
         <div>
-           <input id="firstName" name="firstName" type="text" placeholder="Enter your name" />
+           <input id="firstName" name="firstName" type="text" placeholder="Enter your name" onChange={(e) => setFirstName(e.target.value)} />
         </div>
     </form>
     
