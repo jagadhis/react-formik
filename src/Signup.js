5baa41933972a378b37c55731256d3d78c8923a0
jagadhis <jwaran78@gmail.com>
2022-10-10 18:37:08 +0530

    Added New Field

--- src/Signup.js
+++ src/Signup.js
@@ -2,13 +2,15 @@ import React, { useState } from 'react'
 
 function Signup() {
 
-  const [firstName,setFirstName] = useState("")
+  const [firstName,setFirstName] = useState("");
+  const [lastName,setLastName] = useState("");
 
   console.log(firstName);
   return (
     <form>
         <div>
            <input id="firstName" name="firstName" type="text" placeholder="Enter your name" onChange={(e) => setFirstName(e.target.value)} />
+           <input id="lastName" name="lastName" type="text" placeholder="Enter your name" onChange={(e) => setLastName(e.target.value)} />
         </div>
     </form>
     
