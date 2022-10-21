9e5a42781f48ac4db70f73472d3d2af8e5e5a44a
jagadhis <jwaran78@gmail.com>
2022-10-21 17:58:22 +0530

    Changes

--- src/Signup.js
+++ src/Signup.js
@@ -16,7 +16,14 @@ function Signup() {
 
   return (
     <form onSubmit={formik.handleSubmit}>
-     
+     <label htmlFor='firstName'>FirstName : </label>
+     <input 
+     id="firstName"
+     name="firstName"
+     type="text"
+     onChange={formik.handleChange}
+     value={formik.values.firstName}
+     />
     </form>
     
   )
