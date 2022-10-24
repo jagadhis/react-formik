0b975fc3d6fc88f2babe049bf861e803bfe6f2a9
jagadhis <jwaran78@gmail.com>
2022-10-24 20:23:07 +0530

    Added Route To the Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Signup from './Signup';
 import Home from './Home';
@@ -6,8 +7,12 @@ import Home from './Home';
 function App() {
   return (
     <div className="App">
-      <Home />
-      <Signup />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+
+      </Routes>
+
     </div>
   );
 }
