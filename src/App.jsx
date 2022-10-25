f60b3980f58a233e815e4f6408fe38ea7b45d806
jagadhis <jwaran78@gmail.com>
2022-10-25 17:44:29 +0530

    Changing File Structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Signup from './Signup';
-import Home from './Home';
+import Home from './Navbar';
 
 function App() {
   return (
